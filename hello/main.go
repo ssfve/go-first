2e8245d00612983fdc8fad4469fa15f34ea8250d
ElvinLin <ssfvessfve@163.com>
2024-03-11 12:18:18 +0800

    first commit

---
+++ hello/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	main1 "go-first"
+	_ "go-first/func1"
+	"log"
+)
+
+func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	//message, err := ct.Hello("")
+	message, err := main1.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
+}
